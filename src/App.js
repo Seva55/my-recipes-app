6863a96fda8e6906329d477c134e7c07df4c8d78
seva55 <mirseva55@gmail.com>
2021-10-04 19:39:51 -0700

    Router

--- src/App.js
+++ src/App.js
@@ -9,19 +9,24 @@ import Login from './components/login/Login';
 import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 import CreateComponent from "./components/crud/Create";
 import fire from './fire';
+import PrivateRoute from './components/routes/PrivateRoute';
+import PublicRoute from './components/routes/PublicRoute';
+
 
 function App() {
+
+
   return (
     <div className="App">
       <Router>
       <Navbar fire={fire}/>
       <Switch>
-        <Route exact path="/" component={() => <HomePage fire={fire}/>}/>
-        <Route exact path="/create/:id" component={() => <CreateComponent fire={fire} />}/>
-        <Route exact path="/create" component={() => <CreateComponent fire={fire} />}/>
-        <Route exact path="/homePage" component={() => <HomePage fire={fire}/>}/>
-        <Route exact path="/categories" component={Categories}/>
-        <Route exact path="/random" component={() => <RandomMeal fire={fire}/>}/>
+        <PublicRoute exact path="/login"  component={() => <HomePage fire={fire}/>}/>
+        <PrivateRoute exact path="/create/:id"  component={() => <CreateComponent fire={fire} />}/>
+        <PrivateRoute exact path="/create"  component={() => <CreateComponent fire={fire} />}/>
+        <PublicRoute exact path="/homePage"  component={() => <HomePage fire={fire}/>}/>
+        <PrivateRoute exact path="/categories"  component={Categories}/>
+        <PrivateRoute exact path="/random"  component={() => <RandomMeal fire={fire}/>}/>
         <Route exact path="/login" component={() => <Login fire={fire}/>}/>
       </Switch>
       <Footer />
